6e2a7c035bff17d228f1171d1ac4b1567d0eef30
Marek Lewandowski <marek@mlewandowski.com>
2017-01-29 22:02:30 +0100

    Added couple Tokenizer tests.

---
+++ test/Tokenizer.test.js
@@ -0,0 +1,24 @@
+const Tokenizer = require( '../src/Tokenizer' );
+
+describe( 'Tokenizer', () => {
+	let mock = new Tokenizer();
+
+	it( 'has correct RTF_NEW_LINE', () => {
+		expect( Tokenizer.RTF_NEW_LINE ).to.be.equal( '\r\n' );
+	} );
+
+	describe( 'splitRegExp', () => {
+		it( 'splits multiline string', () => {
+			let str = 'foo bar\r\nbaz\r\n\r\nboom';
+
+			expect( str.split( mock.splitRegExp ) ).to.be.eql( [ 'foo bar', 'baz', '', 'boom' ] );
+		} );
+	} );
+
+
+	describe('process', () => {
+		it('returns a valid value', () => {
+			mock.process( '{\rtf1 foobar}' );
+		});
+	});
+} );
