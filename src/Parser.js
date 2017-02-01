3ef097a1e8215a5774e4da1db5e0bd4f34bfea26
Marek Lewandowski <marek@mlewandowski.com>
2017-02-01 03:27:43 +0100

    Fixed #8 - Introduced contextChanged event.

--- src/Parser.js
+++ src/Parser.js
@@ -2,21 +2,32 @@
 	'use strict';
 
 	const Document = require( './rtf/model/Document' ),
-		Tokenizer = require( './Tokenizer' );
+		Tokenizer = require( './Tokenizer' ),
+		EventEmmiter = require( 'events' );
 
-	class Parser {
+	class Parser extends EventEmmiter {
 		parseString( str ) {
 			return new Promise( ( resolve, reject ) => {
 				let doc = new Document(),
 					tokenizer = new Tokenizer(),
 					modelContext = doc;
 
-				tokenizer.on( 'matched', token => modelContext = token.applyToModel( modelContext ) || modelContext );
+				tokenizer.on( 'matched', token => modelContext = this._tokenMatched( token, modelContext ) );
 
 				let tokens = tokenizer.process( str );
 
 				resolve( doc );
 			} );
+		}
+
+		_tokenMatched( token, currentContext ) {
+			let targetContext = token.applyToModel( currentContext ) || currentContext;
+
+			if ( targetContext !== currentContext ) {
+				this.emit( 'contextChanged', targetContext, currentContext );
+			}
+
+			return targetContext;
 		}
 	};
 
--- test/Parser.test.js
+++ test/Parser.test.js
@@ -1,29 +1,57 @@
 const fsp = require( 'fs-promise' ),
 	Parser = require( '../src/Parser' ),
-	DocumentModel = require( '../src/rtf/model/Document' );
+	DocumentModel = require( '../src/rtf/model/Document' ),
+	GroupModel = require( '../src/rtf/model/Group' );
 
 describe( 'Parser', () => {
-	let parserMock = new Parser();
+	let parserMock;
+
+	beforeEach( () => {
+		parserMock = new Parser();
+	} );
 
 	describe( 'parseString', () => {
+		let readSimpleRtf = () => fsp.readFile( path.join( __dirname, '_fixtures', 'rtfSimple.rtf' ), {
+			encoding: 'utf8'
+		} );
+
 		it( 'returns a promise', () => {
-			return fsp.readFile( path.join( __dirname, '_fixtures', 'rtfSimple.rtf' ), {
-					encoding: 'utf8'
-				} )
+			return readSimpleRtf()
 				.then( content => {
 					expect( parserMock.parseString( content ) ).to.be.instanceof( Promise );
 				} );
 		} );
 
 		it( 'resolves simple rtf', () => {
-			return fsp.readFile( path.join( __dirname, '_fixtures', 'rtfSimple.rtf' ), {
-					encoding: 'utf8'
-				} )
+			return readSimpleRtf()
 				.then( content => parserMock.parseString( content ) )
 				.then( doc => {
 					expect( doc ).to.be.instanceof( DocumentModel );
 				} );
 		} );
-	} );
 
+		it( 'emits contextChanged events', () => {
+			let listener = sinon.spy();
+			parserMock.on( 'contextChanged', listener );
+
+			return fsp.readFile( path.join( __dirname, '_fixtures', 'rtfNested.rtf' ), {
+					encoding: 'utf8'
+				} )
+				.then( content => parserMock.parseString( content ) )
+				.then( doc => {
+					expect( listener ).to.be.called;
+					expect( listener ).to.be.calledWith( sinon.match.instanceOf( GroupModel ), sinon.match.instanceOf( GroupModel ) );
+				} );
+		} );
+
+		it( 'does not fire contextChanged event if no change occurred', () => {
+			let listener = sinon.spy();
+			parserMock.on( 'contextChanged', listener );
+
+			return parserMock.parseString( 'foobar' )
+				.then( doc => {
+					expect( listener ).not.to.be.called;
+				} );
+		} );
+	} );
 } );
