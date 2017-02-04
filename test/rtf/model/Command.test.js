3551e61571655af63be9aeaa9eca4fdd2b5e1dc3
Marek Lewandowski <marek@mlewandowski.com>
2017-02-04 19:30:58 +0100

    Added tests.

---
+++ test/rtf/model/Command.test.js
@@ -0,0 +1,28 @@
+const Command = require( '../../../src/rtf/model/Command' );
+
+describe( 'Command', () => {
+	let mock;
+
+	beforeEach( () => {
+		mock = new Command();
+	} );
+
+	describe( 'value setter', () => {
+		it( 'sets name correctly', () => {
+			mock.value = '\\foo123 ';
+			expect( mock.name ).to.be.equal( 'foo123' );
+
+			mock.value = '\\a';
+			expect( mock.name ).to.be.equal( 'a' );
+		} );
+	} );
+
+
+	describe( 'value getter', () => {
+		it( 'returns correct value', () => {
+			mock.value = 'aa';
+			expect( mock.value ).to.be.equal( 'aa' );
+
+		} );
+	} );
+} );
