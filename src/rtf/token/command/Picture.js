2e077210c778c206c1629e272e330720b1849a97
Marek Lewandowski <marek@mlewandowski.com>
2017-02-05 03:14:38 +0100

    Remvoed code leftovers.

--- src/rtf/token/command/Picture.js
+++ src/rtf/token/command/Picture.js
@@ -13,13 +13,9 @@
 
 		applyToModel( model ) {
 			let modelParent = model.getParent();
-			// console.log( 'requrested image', this.value );
-			// console.log( modelParent );
-			// console.log( modelParent.getChild( child => console.log( child, child instanceof Command, child.name === 'nonshppict' ) && false, true ) );
-			// console.log( modelParent.getChild( child => child instanceof Command && child.name === 'nonshppict', true ) );
+
 			// We don't want to create pictures that are children of nonshppict, as it's a deprecated construction.
 			if ( !modelParent || !modelParent.getChild( child => child.name === 'nonshppict', true ) ) {
-				console.log( 'returning correct pict' );
 				model.append( new PictureModel( this.value ) );
 			} else {
 				return super.applyToModel( model );
