1dbe794a871b938c70858fd5887e43f6baadd901
Marek Lewandowski <marek@mlewandowski.com>
2017-02-07 00:32:53 +0100

    Fixed #15 - Exposed model namespace in index file.

--- test/index.test.js
+++ test/index.test.js
@@ -6,4 +6,8 @@ describe( 'rtfParse', () => {
 		expect( rtfParse ).to.be.instanceof( Parser );
 	} );
 
+	it( 'exposes model types', () => {
+		expect( rtfParse.model.Document ).to.be.instanceOf( Function );
+		expect( rtfParse.model.Command ).to.be.instanceOf( Function );
+	} );
 } );
